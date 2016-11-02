0ee21c9db4cab8e4c7e6ad95ccfc4c0ba08a99ae
ytworks <yuzo.takagi@healthdataplatform.co.jp>
2016-11-02 14:43:42 +0900

    release

--- dcgan.py
+++ dcgan.py
@@ -272,9 +272,9 @@ if __name__ == '__main__':
     config["TrainingConfig"]["LearningBeta1"] = 0.5
     dnn = dcgan(config = config, feature_match = 0.1)
     dnn.construct()
-    learning_config = {'BatchConfig' : {'TrainNum' : 3,
+    learning_config = {'BatchConfig' : {'TrainNum' : 100000,
                                         'BatchSize' : 50,
-                                        'LogPeriod' : 1}}
+                                        'LogPeriod' : 10}}
     dnn.learning(data = data, config = learning_config, boost = 1)
     z = [2.0 * np.random.rand(100) - 1.0]
     img = dnn.get_image(z = z)
